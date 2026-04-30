8e0fad9167499b4c0ea5af67de8d4b4e932cba3d
olyme <olyme111@naver.com>
2026-04-30 22:11:55 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int i = 6;
+    while(i % n != 0)
+    {
+        i += 6;
+    }
+    
+    return i / 6;
+}
