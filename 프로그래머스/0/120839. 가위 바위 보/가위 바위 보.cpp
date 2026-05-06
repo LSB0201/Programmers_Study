8a50214bbcb15e35839821020f52da0ac66c93ec
olyme <olyme111@naver.com>
2026-05-07 00:40:32 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string rsp) {
+    string answer = "";
+    for(char i : rsp)
+    {
+        switch (i)
+        {
+            case '0':
+                answer +=  '5';
+                break;
+                
+            case '2':
+                answer +=  '0';
+                break;
+                
+            case '5':
+                answer +=  '2';
+                break;
+        }
+    }
+    
+    return answer;
+}
