83c1de89c906e634de0deda43ba874a6e197f03d
olyme <olyme111@naver.com>
2026-05-09 23:19:47 +0900

    [level 0] Title: 소인수분해, Time: 0.04 ms, Memory: 3.98 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    vector<bool> prime(n+1, true);
+    prime[0] = prime[1] = false;
+    
+    for (int i = 2; i * i <= n; ++i)
+    {
+        if (prime[i])
+        {
+            for (int j = i * i; j <= n; j += i)
+            {
+                prime[j] = false;
+            }
+        }
+    }
+    
+    for (int i = 2; i <= n; ++i)
+    {
+        if (prime[i] && (n % i == 0))
+        {
+            answer.push_back(i);
+        }
+    }
+    
+    return answer;
+}
