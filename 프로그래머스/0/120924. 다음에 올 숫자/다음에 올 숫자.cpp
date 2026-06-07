3d43cecbde36f9f9f26baec8ccbe1e2cf726a555
olyme <olyme111@naver.com>
2026-06-07 20:31:09 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.01 ms, Memory: 4.06 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> common) {
+    int answer = 0;
+    
+    if (common[1] - common[0] == common[2] - common[1]) // 등차
+    {
+        answer = common.back() + (common[1] - common[0]);
+    }
+    else // 등비
+    {
+        answer = common.back() * (common[1] / common[0]);
+    }
+    
+    return answer;
+}
